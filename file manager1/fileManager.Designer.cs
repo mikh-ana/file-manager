e5fa7543b885a677c899ce83eaf8795c49a65418
mikh-ana <79360764+mikh-ana@users.noreply.github.com>
2021-05-21 17:16:01 +0300

    Добавьте файлы проекта.

---
+++ file manager1/fileManager.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace file_manager1
+{
+    partial class fileManager
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fileManager));
+            this.listDir = new System.Windows.Forms.ListBox();
+            this.addressDir = new System.Windows.Forms.TextBox();
+            this.goDir = new System.Windows.Forms.Button();
+            this.backDir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listDir
+            // 
+            this.listDir.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listDir.FormattingEnabled = true;
+            this.listDir.ItemHeight = 16;
+            this.listDir.Location = new System.Drawing.Point(0, 78);
+            this.listDir.Name = "listDir";
+            this.listDir.Size = new System.Drawing.Size(800, 372);
+            this.listDir.TabIndex = 0;
+            this.listDir.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listDir_MouseDoubleClick);
+            // 
+            // addressDir
+            // 
+            this.addressDir.Location = new System.Drawing.Point(12, 12);
+            this.addressDir.Name = "addressDir";
+            this.addressDir.Size = new System.Drawing.Size(696, 20);
+            this.addressDir.TabIndex = 1;
+            // 
+            // goDir
+            // 
+            this.goDir.Location = new System.Drawing.Point(715, 12);
+            this.goDir.Name = "goDir";
+            this.goDir.Size = new System.Drawing.Size(75, 20);
+            this.goDir.TabIndex = 2;
+            this.goDir.Text = "перейти";
+            this.goDir.UseVisualStyleBackColor = true;
+            this.goDir.Click += new System.EventHandler(this.goDir_Click);
+            // 
+            // backDir
+            // 
+            this.backDir.Location = new System.Drawing.Point(13, 39);
+            this.backDir.Name = "backDir";
+            this.backDir.Size = new System.Drawing.Size(75, 23);
+            this.backDir.TabIndex = 3;
+            this.backDir.Text = "назад";
+            this.backDir.UseVisualStyleBackColor = true;
+            this.backDir.Click += new System.EventHandler(this.backDir_Click);
+            // 
+            // fileManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.backDir);
+            this.Controls.Add(this.goDir);
+            this.Controls.Add(this.addressDir);
+            this.Controls.Add(this.listDir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fileManager";
+            this.Text = "fileManager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listDir;
+        private System.Windows.Forms.TextBox addressDir;
+        private System.Windows.Forms.Button goDir;
+        private System.Windows.Forms.Button backDir;
+    }
+}
+
---
+++ file manager1/fileManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;//для того чтобы запускать процессы
+
+namespace file_manager1
+{
+    public partial class fileManager : Form
+    {
+        public fileManager()
+        {
+            InitializeComponent();
+        }
+        //загрузка файлов и папок-кнопка "перейти"
+        private void goDir_Click(object sender, EventArgs e)
+        {
+            listDir.Items.Clear();//чтобы при загрузки новой директории и это не наслаивалось, поэтому отчищаем
+            DirectoryInfo dir = new DirectoryInfo(addressDir.Text);//для того чтобы работать с папками и файлами
+            DirectoryInfo[] dirs = dir.GetDirectories();//массив каталогов, в этот массив каталогов мы положим все папки которые есть в дир
+            foreach(DirectoryInfo currentDir in dirs)
+            {
+                listDir.Items.Add(currentDir);   //мы в наш лист добавляем элемент
+            }
+            FileInfo[] files = dir.GetFiles();//масиив файлов, чтобы потом его отобразить
+            foreach(FileInfo currentFile in files)
+            {
+                listDir.Items.Add(currentFile);//получаем файлы в лист
+            }
+        }
+
+        private void listDir_MouseDoubleClick(object sender, MouseEventArgs e)
+        { if (Path.GetExtension(Path.Combine(addressDir.Text + listDir.SelectedItem.ToString())) == "")//тут он проверяет есть ли у файла расширение
+            {
+                addressDir.Text = Path.Combine(addressDir.Text + listDir.SelectedItem.ToString());//пользователь кликает на какой-то элемент,
+                                                                                               //этот элемент выделяется и дальше мы добавляем к адресной строке его имя
+                listDir.Items.Clear();                                                        //у класса Path метод комбайн - это составление адреса из набора каких-то строк
+                DirectoryInfo dir = new DirectoryInfo(addressDir.Text);
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                foreach (DirectoryInfo crrDir in dirs)
+                {
+                    listDir.Items.Add(crrDir);
+                }
+                FileInfo[] files = dir.GetFiles();
+                foreach (FileInfo crrFile in files)
+                {
+                    listDir.Items.Add(crrFile);
+                }
+            }
+        else
+            {
+                Process.Start(Path.Combine(addressDir.Text + listDir.SelectedItem.ToString()));//процесс это класс систем диагностикс
+            }
+        }
+            
+
+        private void backDir_Click(object sender, EventArgs e)
+        {
+            if (addressDir.Text[addressDir.Text.Length - 1] == '\\')//когда есть слеш после адреса, -1, потому что в массивах нумерациЯ с нуля
+            {
+                addressDir.Text = addressDir.Text.Remove(addressDir.Text.Length - 1, 1);//мы сначала стираем слеш
+                while (addressDir.Text[addressDir.Text.Length - 1] != '\\')//работаем со строкой как с массивом
+                {
+                    addressDir.Text = addressDir.Text.Remove(addressDir.Text.Length - 1, 1);//цикл файл работает до ней пор пока не встретит слеш
+                }
+
+            }
+            else if (addressDir.Text[addressDir.Text.Length - 1] != '\\')//когда нет слеша, мы просто стираем адресную строку, пока не будет встречен слеш
+            {
+                addressDir.Text = addressDir.Text.Remove(addressDir.Text.Length - 1, 1);
+            }
+            listDir.Items.Clear();             //и потом загружаем содержимое папки
+            DirectoryInfo dir = new DirectoryInfo(addressDir.Text);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            foreach (DirectoryInfo crrDir in dirs)
+            {
+                listDir.Items.Add(crrDir);
+            }
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo crrFile in files)
+            {
+                listDir.Items.Add(crrFile);
+            }
+
+
+        }
+    }
+}
